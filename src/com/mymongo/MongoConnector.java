be2a2a12a1989ca8d4e6dd589f94ababc8d63b7f
Farhan Rahman <farhanrahman26@gmail.com>
2012-06-12 00:32:14 +0100

    example json added

--- src/com/mymongo/MongoConnector.java
+++ src/com/mymongo/MongoConnector.java
@@ -4,19 +4,11 @@
 package com.mymongo;
 
 import java.net.UnknownHostException;
-import java.util.List;
 import java.util.Set;
 
-import net.sf.json.JSONObject;
-
-import com.data.Countries;
-import com.data.CountryData;
 import com.data.SimulationData;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
@@ -123,46 +115,13 @@ public class MongoConnector {
         
         while(cur.hasNext()) {
         	DBObject ob = cur.next();
-            //System.out.println(ob.toString());
         	Gson gson = new GsonBuilder().create();
         	String json = JSON.serialize(ob);
-        	//System.out.println(json);
+        	System.out.println(json);
 			SimulationData data = gson.fromJson(json, SimulationData.class);
 		    
 			System.out.println(data.toString());        	
         }
-        
-//        List<DBObject> obj = collection.find(query).toArray();
-//        for(DBObject d : obj){
-//        	Gson gson = new GsonBuilder().create();
-//			
-//			//String message = gson.fromJson(array.get(0), String.class);
-//			Countries data = gson.fromJson(json, Countries.class);        	
-//        }
-//        
-       /* for(DBObject d : obj){
-        	//System.out.println(d);
-        	for(String key : d.keySet()){
-        		//if(key.equals("countries")){
-        		//	Country c = new Gson().fromJson(d.get(key).toString(), Country.class);
-        		//	c.output();
-        		//}
-        		//String json = "{ 'Type' : 'Group 1/2' , 'name' : 'Australia' , 'ISO' : 'AUS' , 'agentName' : 'EU' , 'Ratified' : '39419' , 'totalArea' : '7692024' , 'landArea' : '7682300' , 'waterArea' : '58920' , 'arableLandArea' : '468503' , 'GDP' : '3.00893E+11' , 'GDPRate' : '0' , 'availableToSpend' : '0' , 'emissionsTarget' : '305994' , 'carbonOffset' : '0' , 'energyOutput' : '423248' , 'carbonOutput' : '0'}";
-        		//String json = "{\"AUS\":{ \"Type\" : \"Group 1/2\" , \"name\" : \"Australia\" , \"ISO\" : \"AUS\" , \"agentName\" : \"EU\" , \"Ratified\" : \"39419\" , \"totalArea\" : \"7692024\" , \"landArea\" : \"7682300\" , \"waterArea\" : \"58920\" , \"arableLandArea\" : \"468503\" , \"GDP\" : \"3.00893E+11\" , \"GDPRate\" : \"0\" , \"availableToSpend\" : \"0\" , \"emissionsTarget\" : \"305994\" , \"carbonOffset\" : \"0\" , \"energyOutput\" : \"423248\" , \"carbonOutput\" : \"0\"}}";
-                          
-        		if(key.equals("countries")){
-        			//System.out.println(d.get(key));
-        			System.out.println(d.get(key));
-        			Gson gson = new GsonBuilder().create();
-        			String json = "{'countries':{'AUS':{ 'Type' : 'Group 1/2' , 'name' : 'Australia' , 'ISO' : 'AUS' , 'agentName' : 'EU' , 'Ratified' : '39419' , 'totalArea' : '7692024' , 'landArea' : '7682300' , 'waterArea' : '58920' , 'arableLandArea' : '468503' , 'GDP' : '3.00893E+11' , 'GDPRate' : '0' , 'availableToSpend' : '0' , 'emissionsTarget' : '305994' , 'carbonOffset' : '0' , 'energyOutput' : '423248' , 'carbonOutput' : '0'},'CHN' : { 'Type' : 'Group 3' , 'name' : 'China, Peoples Republic of' , 'ISO' : 'CHN' , 'agentName' : 'NonAnnexOne' , 'Ratified' : '37498' , 'totalArea' : '9596961' , 'landArea' : '9569901' , 'waterArea' : '27060' , 'arableLandArea' : '1385905', 'GDP' : '3.97467E+11' , 'GDPRate' : '0' , 'availableToSpend' : '0' , 'emissionsTarget' : '3259619' , 'carbonOffset' : '0' , 'energyOutput' : '8094626' , 'carbonOutput' : '0'}}}";
-        			
-        			//String message = gson.fromJson(array.get(0), String.class);
-        			Countries data = gson.fromJson(json, Countries.class);
-        		    
-        			System.out.println(data.toString());
-                }
-        	}
-        }*/
         
 		this.closeConnection();
 	}
